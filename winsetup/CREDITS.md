a98be49969dad18d879a14b4f69bb5670fe988c8
machinateur <hello@machinateur.dev>
2024-11-06 00:13:56 +0100

    Add winsetup files

---
+++ winsetup/CREDITS.md
@@ -0,0 +1,6 @@
+# Thanks for using Tab Transfer
+
+Have a nice day :)
+
+- https://github.com/machinatuer/tab-transfer
+- https://machinateur.dev
---
+++ winsetup/build-setup.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+rem "Assume php, adb and ios-webkit-debug-proxy are already in place and `tab-transfer.cmd` is prepared."
+copy "%cd%\..\tab-transfer.phar" "%cd%\..\tools\tab-transfer.phar"
+copy "%cd%\..\LICENSE"           "%cd%\..\tools\LICENSE"
+
+rem "Assume pandoc to be installed on the system."
+pandoc -s "%cd%\..\README.md" -o "%cd%\..\tools\README.rtf"
+pandoc -s "%cd%\CREDITS.md" -o "%cd%\..\tools\CREDITS.rtf"
+
+rem "Compile `tab-transfer.iss` setup script. You have to modify it from the `.dist` for your system."
+iscc "%cd%\tab-transfer.iss"
